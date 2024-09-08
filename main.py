b650f2404a232d91f88d8f69c57fb37e76ea51c0
parkchanghyup <ariz1623@naver.com>
2024-09-08 15:55:21 +0900

    summarizer

--- main.py
+++ main.py
@@ -4,19 +4,24 @@ import json
 from pathlib import Path
 
 from src.data_processing.json_loader import load_json_data
-from src.data_processing.preprocessor import preprocess_data
+# from src.data_processing.preprocessor import preprocess_data
 from src.summarization.summarizer import generate_summaries
 from src.model.trainer import train_model
 from src.model.inference import run_inference, load_model
+from transformers import AutoModelForCausalLM, AutoTokenizer
 
-def load_config(config_path):
+def load_yaml(config_path):
     with open(config_path, 'r') as file:
         return yaml.safe_load(file)
 
-def summarize(config):
+def summarize(config, prompts):
     raw_data = load_json_data(config['data']['raw_data_path'])
-    processed_data = preprocess_data(raw_data)
-    summaries = generate_summaries(processed_data, config['model'])
+    generate_prompt = prompts['inference']['user']
+
+    model = AutoModelForCausalLM.from_pretrained(config['model']['name'])
+    tokenizer = AutoTokenizer.from_pretrained(config['model']['name'])
+
+    summaries = generate_summaries(raw_data,model, tokenizer, generate_prompt, config)
 
     Path(config['data']['summaries_path']).mkdir(parents=True, exist_ok=True)
     with open(Path(config['data']['summaries_path']) / 'summaries.json', 'w') as f:
@@ -58,23 +63,25 @@ def infer(config):
     print("Inference completed and results saved successfully!")
 
 
-def main(args, config):
+def main(args, config, prompts):
     if args.summarize:
-        summarize(config)
-    elif args.train:
-        train(config)
-    elif args.infer:
-        infer(config)
+        summarize(config, prompts)
+    # elif args.train:
+    #     train(config)
+    # elif args.infer:
+    #     infer(config)
     else:
         print("Please specify an action: --summarize, --train, or --infer")
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Run the LLM-based JSON summarization and sLLM training pipeline")
     parser.add_argument('--config', type=str, default='config/config.yaml', help='Path to the configuration file')
+    parser.add_argument('--prompts', type=str, default='config/prompts.yaml', help='Path to the prompts file')
     parser.add_argument('--summarize', action='store_true', help='Generate summaries')
     parser.add_argument('--train', action='store_true', help='Train the model')
     parser.add_argument('--infer', action='store_true', help='Run inference')
     args = parser.parse_args()
     
-    config = load_config(args.config)
-    main(args, config)
+    config = load_yaml(args.config)
+    prompts = load_yaml(args.prompts)
+    main(args, config, prompts)
